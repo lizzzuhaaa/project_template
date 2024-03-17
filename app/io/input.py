78fdc8ff2d21aa9ab2d70633c45a0f19ccf1e656
lizzzuhaaa <mur.mur.may.li@gmail.com>
2024-03-17 15:47:53 +0200

    input output funcs with docstrings commit

--- app/io/input.py
+++ app/io/input.py
@@ -1,8 +1,19 @@
 def input_from_terminal():
+    """
+    Function to get text from terminal
+    :return:
+    (str): text from terminal
+    """
     return input()
 
 
 def input_from_file(path):
+    """
+    Function to get text from file using builtin functionality
+    :param path: path to the file(str)
+    :return:
+    (str): text from file
+    """
     try:
         with open(path, 'r') as file:
             text = file.read()
@@ -13,6 +24,11 @@ def input_from_file(path):
 
 
 def input_from_file_pandas(path):
+    """
+    Function to get text from csv file using lib pandas functionality
+    :param path: path to the file(str)
+    :return: text from file
+    """
     import pandas
     try:
         data = pandas.read_csv(path)
--- app/io/output.py
+++ app/io/output.py
@@ -1,8 +1,17 @@
 def output_terminal(text):
+    """
+    Function to show given text in terminal
+    :param text: text to show(str)
+    """
     print(text)
 
 
 def output_file(text, path):
+    """
+    Function to write given text in a given file
+    :param text: text to write(str)
+    :param path: path to the file(str)
+    """
     try:
         with open(path, 'w') as file:
             file.write(text)
