3a6bce1908464d119aea8f54607cd5a795f4c8cb
lizzzuhaaa <mur.mur.may.li@gmail.com>
2024-03-17 16:02:14 +0200

    main with tests of input output funcs commit

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from app.io.output import *
 
 
 def main():
-    path = "D:/project_template/data/text.txt"
+    path = "data/text.txt"
     text1 = input_from_terminal()
     output_terminal(text1)
     output_file(text1, path)
@@ -12,7 +12,7 @@ def main():
     output_terminal(text2)
     output_file(text2, path)
 
-    path = "D:/project_template/data/text.csv"
+    path = "data/text.csv"
     text2 = input_from_file_pandas(path)
     output_terminal(text2)
     output_file(text2, path)
