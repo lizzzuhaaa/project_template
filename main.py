572b7455e172a22821f9174f3cf0f197cf12f58c
lizzzuhaaa <mur.mur.may.li@gmail.com>
2024-03-17 15:59:35 +0200

    main with tests of input output funcs commit

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
+from app.io.input import *
+from app.io.output import *
+
+
 def main():
-    pass
+    path = "D:/project_template/data/text.txt"
+    text1 = input_from_terminal()
+    output_terminal(text1)
+    output_file(text1, path)
+
+    text2 = input_from_file(path)
+    output_terminal(text2)
+    output_file(text2, path)
+
+    path = "D:/project_template/data/text.csv"
+    text2 = input_from_file_pandas(path)
+    output_terminal(text2)
+    output_file(text2, path)
 
 
 if __name__ == '__main__':
     main()
-
-
